f95be340f97b071a80cf2c532c6e43575bb8a799
Maotou <phucbuihuu2604@gmail.com>
2023-02-15 10:13:39 +0700

    #8 Routers & MVC design pattern
    Video số 8 trong chuỗi video NodeJs basic course of hoidapit

--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 import ConfigViewEngine from './configs/viewEngine'
+import initWebRoutes from './routes/web'
+
 require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 8080;
 
+//Set up view engine
 ConfigViewEngine(app)
+
+//init all web routes
+initWebRoutes(app)
 
 app.get('/hi', (req, res) => {
     res.send('Hello World! vs Maotou')
